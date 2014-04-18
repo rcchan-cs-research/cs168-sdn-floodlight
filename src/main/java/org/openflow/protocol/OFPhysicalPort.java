35ba2ad9bdf1c6bd394a2d8bb2ac68a7295ef5b7
Srini Seetharaman <srini.seetharaman@gmail.com>
2014-04-18 11:14:08 -0700

    Updating OFPhysicalPort to have OFPortSpeed as a separate enum. So we can now track speed, full-duplexity, name and other values for port features.

--- src/main/java/org/openflow/protocol/OFPhysicalPort.java
+++ src/main/java/org/openflow/protocol/OFPhysicalPort.java
@@ -4,6 +4,8 @@ import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 import java.util.Arrays;
+import java.util.Map;
+import org.openflow.util.LRULinkedHashMap;
 
 /**
  * Represents ofp_phy_port
@@ -34,46 +36,119 @@ public class OFPhysicalPort {
     }
 
     public enum OFPortState {
-        OFPPS_LINK_DOWN   (1 << 0),
-        OFPPS_BLOCKED     (1 << 1),
-        OFPPS_LIVE        (1 << 2);
+        OFPPS_LINK_DOWN   (1 << 0, "DOWN"),
+        OFPPS_BLOCKED     (1 << 1, "BLOCKED"),
+        OFPPS_LIVE        (1 << 2, "UP");
 
         protected int value;
+        protected String name;
+        protected static Map<Integer, OFPortState> mapping;
 
-        private OFPortState(int value) {
-            this.value = value;
+        private OFPortState(int value, String name) {
+        	this.value = value;
+            this.name = name;
+
+            OFPortState.addMapping(this.value, this);
         }
 
+        /**
+         * Adds a mapping from type value to OFPortFeatures enum
+         *
+         * @param i state values
+         * @param t type
+         */
+        static public void addMapping(int i, OFPortState t) {
+            if (mapping == null)
+                mapping = new LRULinkedHashMap<Integer, OFPortState>(10);
+
+            mapping.put(i, t);
+        }
+
+        /**
+         * Given a wire protocol OpenFlow type number, return the OFType associated
+         * with it
+         *
+         * @param i state values
+         * @return OFPortFeatures enum type
+         */
+
+        public static OFPortState valueOf(int i) {
+            return mapping.get(i);
+        }
+        
         /**
          * @return the value
          */
         public int getValue() {
             return value;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+    }
+
+    /**
+     * Represents the speed of a port
+     */
+    public enum OFPortSpeed {
+        SPEED_NONE(0),
+        SPEED_10MB(10),
+        SPEED_100MB(100),
+        SPEED_1GB(1000),
+        SPEED_10GB(10000),
+        SPEED_40GB(40000),
+        SPEED_100GB(100000),
+        SPEED_1TB(1000000);
+
+        private int speedInMps;
+
+        private OFPortSpeed(int speedInMbps) {
+            this.speedInMps = speedInMbps;
+        }
+
+        public long getSpeedInMbps() {
+            return this.speedInMps;
+        }
+
+        public static OFPortSpeed max(OFPortSpeed s1, OFPortSpeed s2) {
+            return (s1.getSpeedInMbps() > s2.getSpeedInMbps()) ? s1 : s2;
+        }
+
+        public static OFPortSpeed min(OFPortSpeed s1, OFPortSpeed s2) {
+            return (s1.getSpeedInMbps() < s2.getSpeedInMbps()) ? s1 : s2;
         }
     }
 
     public enum OFPortFeatures {
-        OFPPF_10MB_HD    (1 << 0),
-        OFPPF_10MB_FD    (1 << 1),
-        OFPPF_100MB_HD   (1 << 2),
-        OFPPF_100MB_FD   (1 << 3),
-        OFPPF_1GB_HD     (1 << 4),
-        OFPPF_1GB_FD     (1 << 5),
-        OFPPF_10GB_FD    (1 << 6),
-        OFPPF_40GB_FD    (1 << 7),
-        OFPPF_100GB_FD   (1 << 8),
-        OFPPF_1TB_FD     (1 << 9),
-        OFPPF_OTHER      (1 << 10),
-        OFPPF_COPPER     (1 << 11),
-        OFPPF_FIBER      (1 << 12),
-        OFPPF_AUTONEG    (1 << 13),
-        OFPPF_PAUSE      (1 << 14),
-        OFPPF_PAUSE_ASYM (1 << 15);
+        OFPPF_10MB_HD    (1 << 0, OFPortSpeed.SPEED_10MB, false, "10MB half-duplex"),
+        OFPPF_10MB_FD    (1 << 1, OFPortSpeed.SPEED_10MB, true, "10MB full-duplex"),
+        OFPPF_100MB_HD   (1 << 2, OFPortSpeed.SPEED_100MB, false, "100MB half-duplex"),
+        OFPPF_100MB_FD   (1 << 3, OFPortSpeed.SPEED_100MB, true, "100MB full-duplex"),
+        OFPPF_1GB_HD     (1 << 4, OFPortSpeed.SPEED_1GB, false, "1GB half-duplex"),
+        OFPPF_1GB_FD     (1 << 5, OFPortSpeed.SPEED_1GB, true, "1GB full-duplex"),
+        OFPPF_10GB_FD    (1 << 6, OFPortSpeed.SPEED_10GB, true, "10GB full-duplex"),
+        OFPPF_40GB_FD    (1 << 7, OFPortSpeed.SPEED_40GB, true, "40GB full-duplex"),
+        OFPPF_100GB_FD   (1 << 8, OFPortSpeed.SPEED_100GB, true, "100GB full-duplex"),
+        OFPPF_1TB_FD     (1 << 9, OFPortSpeed.SPEED_1TB, true, "1TB full-duplex"),
+        OFPPF_OTHER      (1 << 10, OFPortSpeed.SPEED_NONE, false, "Other"),
+        OFPPF_COPPER     (1 << 11, OFPortSpeed.SPEED_NONE, false, "Copper"),
+        OFPPF_FIBER      (1 << 12, OFPortSpeed.SPEED_NONE, false, "Fiber"),
+        OFPPF_AUTONEG    (1 << 13, OFPortSpeed.SPEED_NONE, false, "Autoneg"),
+        OFPPF_PAUSE      (1 << 14, OFPortSpeed.SPEED_NONE, false, "Pause"),
+        OFPPF_PAUSE_ASYM (1 << 15, OFPortSpeed.SPEED_NONE, false, "Pause Asymm");
 
         protected int value;
+        protected OFPortSpeed speed;
+        protected boolean isFullDuplex;
+        protected String name;
 
-        private OFPortFeatures(int value) {
+        private OFPortFeatures(int value, OFPortSpeed speed, boolean isFullDuplex, String name) {
             this.value = value;
+            this.speed = speed;
+            this.isFullDuplex = isFullDuplex;
+            this.name = name;
         }
 
         /**
@@ -81,6 +156,25 @@ public class OFPhysicalPort {
          */
         public int getValue() {
             return value;
+        }
+
+        /**
+         * @return the port speed
+         */
+        public OFPortSpeed getPortSpeed() {
+            return speed;
+        }
+
+        /**
+         * @return boolean about full duplex
+         */
+        public boolean isFullDuplex() {
+            return isFullDuplex;
+        }
+
+        @Override
+        public String toString() {
+            return name;
         }
     }
 
